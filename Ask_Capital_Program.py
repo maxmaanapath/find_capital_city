435fccbef909e9c9c10e38a901dc679c371673b8
GotItMyDADDY <maxmaanapath@gmail.com>
2020-12-16 18:25:54 +0700

    Update python file
    Create function for read files

--- Ask_Capital_Program.py
+++ Ask_Capital_Program.py
@@ -1,2 +1,11 @@
 import sys
 from tkinter import Tk, simpledialog, messagebox
+
+def read_from_file():
+    with open('worldcapital.txt', 'r') as file:
+        for line in file:
+            line = line.rstrip('\n')
+            country, capital = line.split('/')
+            country = country.capitalize()
+            capital = capital.capitalize()
+            world_capital[country] = capital
