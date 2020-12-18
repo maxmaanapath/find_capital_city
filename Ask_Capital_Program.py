343fc6e52cee5b01c6b7de3461c869f426072d8a
GotItMyDADDY <maxmaanapath@gmail.com>
2020-12-18 15:30:00 +0700

    Update program
    Edit content in program

--- Ask_Capital_Program.py
+++ Ask_Capital_Program.py
@@ -29,8 +29,7 @@ while True:
         result = world_capital[query_country]
         messagebox.showinfo('Answer','The capital city of ' + query_country + ' is ' + result + '!')
     else:
-        new_capital = simpledialog.askstring('Teach me',
-                                             'I don\'t know the answer. Please teach me. What is the capital city of ' + query_country + '?:')
+        new_capital = simpledialog.askstring('Teach me', 'I don\'t know the answer. Please teach me. What is the capital city of ' + query_country + '?:')
         messagebox.showinfo('Thanks', 'Thank you for teaching me. I will definitely know it next time!')
         new_capital = new_capital.capitalize()
         write_to_file(query_country, new_capital)
