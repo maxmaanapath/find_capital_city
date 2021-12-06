382ea3477923076afdf8d0c23cf431c87710fee0
GotItMyDADDY <maxmaanapath@gmail.com>
2021-12-06 21:33:11 +0700

    Update .py file
    Edit line in .py file

--- askcapitalcity.py
+++ askcapitalcity.py
@@ -3,6 +3,7 @@ from tkinter import Tk, simpledialog, messagebox
 
 def read_from_file():
     with open('worldcapital_test.txt', 'r') as file:
+
         for line in file:
             line = line.rstrip('\n')
             country, capital = line.split('/')
@@ -11,6 +12,7 @@ def read_from_file():
             world_capital[country] = capital
 
 def write_to_file(country_name, capital_name):
+    
     with open('worldcapital_test.txt', 'a') as file:
         file.write('\n')
         file.write(country_name + '/' + capital_name)
@@ -19,12 +21,14 @@ def write_to_file(country_name, capital_name):
 root = Tk()
 root.withdraw()
 world_capital = {}
+
 while True:
     read_from_file()
     simpledialog.askstring
     query_country = ''
     query_country = simpledialog.askstring('Country', 'Type the name of a country: ')
     query_country = query_country.capitalize()
+
     if query_country in world_capital:
         result = world_capital[query_country]
         messagebox.showinfo('Answer','The capital city of ' + query_country + ' is ' + result + '!')
@@ -33,7 +37,9 @@ while True:
         messagebox.showinfo('Thanks', 'Thank you for teaching me. I will definitely know it next time!')
         new_capital = new_capital.capitalize()
         write_to_file(query_country, new_capital)
+
     answer = simpledialog.askstring('Continue', 'Do you want to try again? y/n: ')
+
     if answer == 'n':
         messagebox.showinfo('Thanks','Thank you for playing!')
         root.destroy()
